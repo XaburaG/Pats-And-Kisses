76939a6b6099b208ceeb09f01ac38ab67ea202bc
XaburaG <99965064+XaburaG@users.noreply.github.com>
2022-02-18 15:36:57 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const patBtn = document.getElementById('pat');
+const kissBtn = document.getElementById('kiss');
+const lickBtn = document.getElementById('lick');
+
+
+patBtn.addEventListener('click', () => {
+    alert('*Pat Pat*');
+})
+
+kissBtn.addEventListener('click', () => {
+    alert('*Kisses You*');
+})
+
+lickBtn.addEventListener('click', () => {
+    alert('*Licks You*');
+})
